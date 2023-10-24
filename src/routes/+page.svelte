186d1654bcc1681c9e3114dafc7b226b5d0fef2a
Fabien Etheve <fabienethevepro@gmail.com>
2023-10-24 22:55:45 +0200

    Premier commit : mise en place du projet

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<main class="container" id="home-container">
+    <div class="row">
+        <div class="title-container">
+            <h1 class="text-center">Le livre d'or d'internet</h1>
+        </div>
+        
+    </div>
+</main>
