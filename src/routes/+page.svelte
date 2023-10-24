d316c3007d85a59d8ca269bd91bd6fe09670faab
scotty974 <fabienethevepro@gmail.com>
2023-10-24 23:27:59 +0200

    Ajout du button de navigation + la scss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <main class="container" id="home-container">
     <div class="row">
         <div class="title-container">
-            <h1 class="text-center">Le livre d'or d'internet</h1>
+            <h1 class="text-center">Le livre d'or du web</h1>
+            <a href="/home" id="nav-button">Découvrir le livre</a>
         </div>
         
     </div>
